f305e1ddc6a8d378dd3aa1a83b55b71b3f55de00
houjichao <houjichao@netposa.com>
2019-03-22 00:26:00 +0800

    *测试

--- src/test/java/com/study/page/UnitTest.java
+++ src/test/java/com/study/page/UnitTest.java
@@ -8,6 +8,7 @@ public class UnitTest {
         PmsAnswerQuestions pmsAnswerQuestions = new PmsAnswerQuestions();
         //pmsAnswerQuestions.setQuestionName("11111111");
         //pmsAnswerQuestions.setStudentId("1111111111");
+		pmsAnswerQuestions.setStudentId("1111111111");
         pmsAnswerQuestions.setTeacherId("222222222");
         pmsAnswerQuestions.setId("a8f4361a-e615-49d8-863a-e3484b278030");
         pmsAnswerQuestions.setAnswer("2222222222");
